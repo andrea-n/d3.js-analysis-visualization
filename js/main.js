024dd35ecbfde4f6ea839e99cc9f94c492faa5d4
Andrea Navratilova <andrea.navratilova@portadesign.cz>
2017-05-15 09:34:47 +0200

    game data increment

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var visualization = {
 	drawPlan: function(config) {
 		element = config.element,
 		colors = config.colors,
-		data = config.data,
+		plandata = config.data,
 		time = config.time,
 		margin = { top: 20, right: 20, bottom: 30, left: 50 },
 		width = 900 - margin.left - margin.right,
@@ -19,18 +19,21 @@ var visualization = {
 				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 		var stack = d3.stack()
-			.keys(data.keys)
+			.keys(plandata.keys)
 			.offset(d3.stackOffsetNone);
 
-		var layers = stack(data.teams);
-		yScale.domain(data.teams.map(function(d) { return d.team; }));
-		xScale.domain([0, d3.max(layers[layers.length - 1], function(d) { return d[1]; }) ]);
-		yScale.domain(data.teams.map(function(d) { return d.team; }));	
+		var layers = stack(plandata.teams);
+
+		planDomain = d3.max(layers[layers.length - 1], function(d) { return d[1]; });
+
+		yScale.domain(plandata.teams.map(function(d) { return d.team; }));
+		xScale.domain([0, planDomain]);
+		yScale.domain(plandata.teams.map(function(d) { return d.team; }));	
 
 		// create hatched pattern defs
 		defs = svg.append("defs");
 		var pattern = defs.selectAll("pattern")
-			.data(data.keys)
+			.data(plandata.keys)
 			.enter().append("pattern")
 			.attr("id", function(d, i) { return "diagonalHatch"+i; })
 			.attr("patternUnits", "userSpaceOnUse")
@@ -45,10 +48,10 @@ var visualization = {
 			.attr("opacity", "0.3");
 
 		// create segment column for each level
-		var layer = svg.selectAll(".layer")
+		var layer = svg.selectAll(".plan-layer")
 			.data(layers)
 			.enter().append("g")
-			.attr("class", "layer")
+			.attr("class", "plan-layer")
 			.style("fill", function(d, i) { return "url(#diagonalHatch" + i +")"; });
 
 		// draw segment for each team
@@ -83,14 +86,52 @@ var visualization = {
 			.call(xAxis);*/
 
 		// append time axis
-		svg.append("line")
-			.attr("class", "time")
-			.attr("x1", xScale(time))
+		timeline = svg.append("line")
+			.attr("class", "timeline")
+			.attr("x1", xScale(time)+2)
 			.attr("y1", 0)
-			.attr("x2", xScale(time))
+			.attr("x2", xScale(time)+2)
 			.attr("y2", height)
 			.attr("stroke-width", 3)
 			.attr("stroke", "#000");
+	},
+	drawData: function(config) {
+		time = config.time,
+		// TODO separate plan/game data colors
+		colors = config.colors,
+		gamedata = config.data,
+		getColor = d3.scaleOrdinal(colors);
+
+		var stack = d3.stack()
+			.keys(gamedata.keys)
+			.offset(d3.stackOffsetNone);
+
+		var layers = stack(gamedata.teams);
+
+		gameDomain = d3.max(layers[layers.length - 1], function(d) { return d[1]; });
+
+		// TODO rescale graph when needed (gameDomain > planDomain)
+		xScale.domain([0, Math.max(planDomain, gameDomain)]);
+
+		// create segment column for each level
+		var layer = svg.selectAll(".game-layer")
+			.data(layers)
+			.enter().append("g")
+			.attr("class", "game-layer")
+			.attr("fill", function(d, i) { return getColor(i); })
+
+		// draw segment for each team
+		layer.selectAll("rect.game-segment")
+		  .data(function(d) {return d; })
+		  .enter().append("rect")
+		  .attr("y", function(d) { return yScale(d.data.team); })
+		  .attr("x", function(d) { return xScale(d[0]); })
+		  .attr("height", yScale.bandwidth())
+		  .attr("width", function(d) { return xScale(d[1]) - xScale(d[0]) });
+
+		 // update time line
+		 timeline.attr("x1", xScale(time)+2)
+		 	.attr("x2", xScale(time)+2);
 	}
 }
 
@@ -102,5 +143,18 @@ d3.json("data/game-plan.json", function(data) {
 		element: 'chart',
 		colors: colors,
 		time: 0
+	});
+});
+
+// TODO: some callback when plan is drawn?
+// TODO: styles attributes to classes
+// TODO: clean it and make it nice
+d3.json("data/game-data.json", function(data) {
+	var gamedata = data;
+	colors = ["#0e6f90", "#158136", "#ec7e26", "#d82f36"];
+	visualization.drawData({
+		data: gamedata,
+		colors: colors,
+		time: gamedata.time
 	});
 });
